c501f33f25cca5ae1a0648991d436f471dc5e18f
DHLau <ldhtrythebest@163.com>
2025-05-29 10:40:40 +0900

    hard  login logic

---
+++ lib/data/auth/repository/auth_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce/data/auth/models/user_creation_req.dart';
+import 'package:ecommerce/data/auth/source/auth_firebase_service.dart';
+import 'package:ecommerce/domain/auth/repository/auth.dart';
+import 'package:ecommerce/service_locator.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  // 依頼注入
+  @override
+  Future<Either> signup(UserCreationReq user) async {
+    return sl<AuthFirebaseService>().signup(user);
+  }
+}
---
+++ lib/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce/data/auth/repository/auth_repository_impl.dart';
+import 'package:ecommerce/data/auth/source/auth_firebase_service.dart';
+import 'package:ecommerce/domain/auth/repository/auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ecommerce/domain/auth/usecases/signup.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+  // Services
+  sl.registerSingleton<AuthFirebaseService>(AuthFirebaseserviceImpl());
+
+  // Responsitories
+  sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+
+  // UserCases
+  sl.registerSingleton<SignupUsecase>(SignupUsecase());
+}
