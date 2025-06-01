f35148595970593f5a331ce808557cb8c8b2284d
DHLau <ldhtrythebest@163.com>
2025-06-01 10:17:21 +0900

    UserInfoDisplayCubit

--- lib/presentation/home/bloc/user_info_display_state.dart
+++ lib/presentation/home/bloc/user_info_display_state.dart
@@ -1,7 +1,12 @@
+import 'package:ecommerce/domain/auth/entity/user_entity.dart';
+
 abstract class UserInfoDisplayState {}
 
 class UserInfoLoading extends UserInfoDisplayState {}
 
-class UserInfoLoaded extends UserInfoDisplayState {}
+class UserInfoLoaded extends UserInfoDisplayState {
+  final UserEntity user;
+  UserInfoLoaded({required this.user});
+}
 
-class UserInfoFailure extends UserInfoDisplayState {}
+class UserInfoLoadFailure extends UserInfoDisplayState {}
