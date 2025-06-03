f974e51461b16d7beb68839d557c2c5f7a8aa264
DHLau <ldhtrythebest@163.com>
2025-06-03 13:10:30 +0900

    fetch topselling data with condision

--- lib/data/product/source/product_firebase_service.dart
+++ lib/data/product/source/product_firebase_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class ProductFirebaseService {
@@ -6,8 +7,18 @@ abstract class ProductFirebaseService {
 
 class ProductFirebaseServiceImpl implements ProductFirebaseService {
   @override
-  Future<Either> getTopSelling() {
-    // TODO: implement getTopSelling
-    throw UnimplementedError();
+  Future<Either> getTopSelling() async {
+    try {
+      var returnData = await FirebaseFirestore.instance
+          .collection('Products')
+          .where(
+            'salesNumber',
+            isGreaterThanOrEqualTo: 2,
+          )
+          .get();
+      return Right(returnData);
+    } catch (e) {
+      return Left("Please try again later");
+    }
   }
 }
