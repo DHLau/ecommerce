613edcbaf2779fb342919604e3179ed596626eda
DHLau <ldhtrythebest@163.com>
2025-06-06 15:40:58 +0900

    order_placed page

--- lib/presentation/cart/pages/order_placed.dart
+++ lib/presentation/cart/pages/order_placed.dart
@@ -1,4 +1,7 @@
 import 'package:ecommerce/common/index.dart';
+import 'package:ecommerce/core/configs/assets/app_images.dart';
+import 'package:ecommerce/core/configs/theme/app_colors.dart';
+import 'package:ecommerce/presentation/home/pages/home.dart';
 import 'package:flutter/material.dart';
 
 class OrderPlacedPage extends StatelessWidget {
@@ -7,11 +10,48 @@ class OrderPlacedPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: BasicAppBar(),
-      body: Scaffold(
-          body: Center(
-        child: Text("OrderPlacedPage"),
-      )),
+      backgroundColor: AppColors.primary,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Image.asset(AppImages.orderPlaced),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 300,
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: const BoxDecoration(
+                  color: AppColors.secondBackground,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Order Placed Successfully',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  BasicAppButton(
+                      title: 'Finish',
+                      onPressed: () {
+                        AppNavigator.pushReplacement(context, const HomePage());
+                      })
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
