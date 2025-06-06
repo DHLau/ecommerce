c1e86de85840e855514f6af23653b3a9334243be
DHLau <ldhtrythebest@163.com>
2025-06-06 15:08:38 +0900

    add input address

--- lib/presentation/cart/pages/check_out.dart
+++ lib/presentation/cart/pages/check_out.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/common/widgets/appbar/app_bar.dart';
 import 'package:ecommerce/domain/order/entity/product_ordered_entity.dart';
 import 'package:flutter/material.dart';
 
@@ -5,12 +6,30 @@ class CheckOutPage extends StatelessWidget {
   final List<ProductOrderedEntity> products;
   CheckOutPage({super.key, required this.products});
 
+  final TextEditingController _addressController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+        appBar: BasicAppBar(
           title: Text('Check Out'),
         ),
-        body: Container());
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              _addressField(context),
+            ],
+          ),
+        ));
+  }
+
+  Widget _addressField(BuildContext context) {
+    return TextField(
+      controller: _addressController,
+      minLines: 2,
+      maxLength: 4,
+      decoration: InputDecoration(hintText: "Shipping Address"),
+    );
   }
 }
